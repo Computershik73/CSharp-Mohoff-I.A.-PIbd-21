c21f97df987d1e254b77c5e1c240f9d205de23ff
Computershik73 <31623340+Computershik73@users.noreply.github.com>
2018-01-03 15:46:15 +0300

    Add files via upload

---
+++ Laba3/Laba2/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba2
+{
+    public partial class Form1 : Form
+    {
+        Color color;
+        Color dopColor;
+        int maxSpeed;
+        int maxCountPass;
+        int maxCountCapa;
+        int weight;
+
+        private ITransport inter;
+        public Form1()
+        {
+            InitializeComponent();
+            color = Color.Red;
+            dopColor = Color.Yellow;
+            maxSpeed = 150;
+            maxCountPass = 4;
+            weight = 1500;
+            buttonSelectColor.BackColor = color;
+            buttonSelectDopColor.BackColor = dopColor;
+        }
+
+        private void buttonSelectColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                color = cd.Color;
+                buttonSelectColor.BackColor = color;
+            }
+        }
+
+        private void buttonSelectDopColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                dopColor = cd.Color;
+                buttonSelectDopColor.BackColor = dopColor;
+            }
+        }
+
+        private bool checkFields()
+        {
+            if (!int.TryParse(textBoxMaxSpeed.Text, out maxSpeed))
+            {
+                return false;
+            }
+            if (!int.TryParse(textBoxMaxCountPassenget.Text, out maxCountPass))
+            {
+                return false;
+            }
+            if (!int.TryParse(textBoxToplivo.Text, out maxCountCapa))
+            {
+                return false;
+            }
+            if (!int.TryParse(textBoxWeight.Text, out weight))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void buttonSetAuto_Click(object sender, EventArgs e)
+        {
+            if (checkFields())
+            {
+                inter = new Military(maxSpeed, maxCountPass, maxCountCapa, weight, color);
+                Bitmap bmp = new Bitmap(pictureBoxDraw.Width, pictureBoxDraw.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                inter.drawCar(gr);
+                pictureBoxDraw.Image = bmp;
+            }
+        }
+
+        private void buttonSetSportSedan_Click(object sender, EventArgs e)
+        {
+            inter = new Tank(150, 4, 7, 1000, Color.Black, true, true, true, Color.Yellow);
+            Bitmap bmp = new Bitmap(pictureBoxDraw.Width, pictureBoxDraw.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            inter.drawCar(gr);
+            pictureBoxDraw.Image = bmp;
+        }
+
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            if (inter != null)
+            {
+                Bitmap bmp = new Bitmap(pictureBoxDraw.Width, pictureBoxDraw.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                inter.moveCar(gr);
+                pictureBoxDraw.Image = bmp;
+            }
+        }
+
+        private void buttonSetGruzovik_Click(object sender, EventArgs e)
+        {
+            if (checkFields())
+            {
+                inter = new Tank(maxSpeed, maxCountPass, maxCountCapa, weight, color, checkBoxFrontSpoiler.Checked,
+                checkBoxBackSpoiler.Checked, checkBoxSideSpoiler.Checked, dopColor);
+                Bitmap bmp = new Bitmap(pictureBoxDraw.Width, pictureBoxDraw.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                inter.drawCar(gr);
+                pictureBoxDraw.Image = bmp;
+            }
+        }
+
+        private void pictureBoxDraw_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBoxСapacity_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    }
---
+++ Laba3/Laba2/Military.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba2
+{
+   public  class Military:Vehicle
+    {
+        
+public override int MaxSpeed
+        {
+            get
+            {
+                return base.MaxSpeed;
+            }
+
+            protected set
+            {
+                if (value > 0 && value < 300)
+                {
+                    base.MaxSpeed = value;
+                }
+                else
+                {
+                    base.MaxSpeed = 150;
+                }
+            }
+        }
+        public override int MaxCountPassengers
+        {
+            get
+            {
+                return base.MaxCountPassengers;
+            }
+
+            protected set
+            {
+                if (value > 0 && value < 5)
+                {
+                    base.MaxCountPassengers = value;
+                }
+                else
+                {
+                    base.MaxCountPassengers = 3;
+                }
+            }
+        }
+        public override int MaxCountToplivo
+        {
+            get
+            {
+                return base.MaxCountToplivo;
+            }
+
+            protected set
+            {
+                if (value > 5 && value < 15)
+                {
+                    base.MaxCountToplivo = value;
+                }
+                else
+                {
+                    base.MaxCountToplivo = 10;
+                }
+            }
+        }
+        public override double Weight
+        {
+            get
+            {
+                return base.Weight;
+            }
+
+            protected set
+            {
+                if (value > 1500 && value < 3000)
+                {
+                    base.Weight = value;
+                }
+                else
+                {
+                    base.Weight = 1500;
+                }
+            }
+        }
+        public Military(int maxSpeed, int maxCountPassengers, int maxCountToplivo, double weight, Color color)
+        {
+            this.MaxSpeed = maxSpeed;
+            this.MaxCountPassengers = maxCountPassengers;
+            this.MaxCountToplivo = maxCountToplivo;
+            this.ColorBody = color;
+            this.Weight = weight;
+            this.countPassengers = 0;
+            this.countToplivo = 0;
+            //Random rand = new Random();
+            //startPosX = rand.Next(10, 200);
+            //startPosY = rand.Next(10, 200);
+        }
+        public override void moveCar(Graphics g)
+        {
+            startPosX +=
+            (MaxSpeed * 50 / (float)Weight) /
+            (countToplivo == 0 ? 1 : countToplivo);
+            drawCar(g);
+        }
+        public override void drawCar(Graphics g)
+        {
+            drawLightCar(g);
+        }
+        protected virtual void drawLightCar(Graphics g)
+        {
+            //границы машины 
+       
+            //кузов 
+            Brush br = new SolidBrush(ColorBody);
+            g.FillRectangle(br, startPosX, startPosY, 150, 100);
+            g.FillRectangle(br, startPosX + 180, startPosY , 30, 100);
+
+            //стекла 
+            Brush brBlue = new SolidBrush(Color.LightBlue);
+            g.FillRectangle(brBlue, startPosX-10, startPosY, 10, 100);
+            g.FillRectangle(brBlue, startPosX + 150, startPosY, 30, 100);
+
+            //выделяем рамкой крышу 
+            Pen pen = new Pen(Color.Black);
+            g.DrawRectangle(pen, startPosX, startPosY, 150, 100);
+            g.DrawRectangle(pen, startPosX + 180, startPosY, 30, 100);
+            g.DrawRectangle(pen, startPosX-10, startPosY, 220, 100);
+
+            //фары
+            Brush brY = new SolidBrush(Color.Yellow);
+            g.FillRectangle(brY, startPosX+200, startPosY, 10, 30);
+            g.FillRectangle(brY, startPosX + 200, startPosY+70, 10, 30);
+            Brush brR = new SolidBrush(Color.Red);
+            g.FillRectangle(brR, startPosX -15, startPosY, 5, 20);
+            g.FillRectangle(brR, startPosX -15, startPosY+80 , 5, 20);
+
+
+        }
+    }
+}
