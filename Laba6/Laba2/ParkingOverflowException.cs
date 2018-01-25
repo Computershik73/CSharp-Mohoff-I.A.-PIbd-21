d82c7ed8b01327c750ae7e6a06a82e35a01f8e03
Computershik73 <il.mohoff2016@yandex.ru>
2018-01-25 08:16:11 +0300

    Лабораторная работа №6

---
+++ Laba6/Laba2/ParkingOverflowException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba2
+{
+    class ParkingOverflowException : Exception
+    {
+        public ParkingOverflowException() :
+        base("На парковке нет свободных мест")
+        { }
+    }
+}
+
